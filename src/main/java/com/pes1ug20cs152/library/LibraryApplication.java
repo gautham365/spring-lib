0b5b1ffbd862d5e33c5f0a2040a4f08a058c6ee0
Gautham <gauthamd.das@gmail.com>
2023-03-30 20:16:47 +0530

    hopefully final

--- src/main/java/com/pes1ug20cs152/library/LibraryApplication.java
+++ src/main/java/com/pes1ug20cs152/library/LibraryApplication.java
@@ -2,18 +2,11 @@ package com.pes1ug20cs152.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EnableJpaRepositories("my.package.base.*")
-@ComponentScan(basePackages = { "my.package.base.*" })
-@EntityScan("my.package.base.*")   
-@SpringBootApplication
-// @ComponentScan(basePackages = { "com.pes1ug20cs152.library.Controller", "com.pes1ug20cs152.library.Service", "com.pes1ug20cs152.library.Model.Book", "com.pes1ug20cs152.library.Repository" })
+@SpringBootApplication()
 public class LibraryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
+
 }
--- src/main/java/com/pes1ug20cs152/library/Service/BookServiceImpl.java
+++ src/main/java/com/pes1ug20cs152/library/Service/BookServiceImpl.java
@@ -1,49 +1,61 @@
 package com.pes1ug20cs152.library.Service;
 
-import java.util.List;
-import org.springframework.stereotype.Service;
+import com.pes1ug20cs152.library.Exception.ResourceNotFoundException;
 import com.pes1ug20cs152.library.Model.Book;
 import com.pes1ug20cs152.library.Repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-// import java.util.Optional;
-// import com.pes1ug20cs152.library.Exception.ResourceNotFoundException;
-// import com.pes1ug20cs152.library.Service.BookService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
+import static org.h2.fulltext.FullTextLucene.search;
 
 @Service
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     @Override
-    public List<Book> getAllBooks() {
+    public List<Book> findAllBooks() {
         return bookRepository.findAll();
     }
 
-    // @Override
-    // public Book getBookById(Long id) {
-    //     Optional<Book> bookOptional = bookRepository.findById(id);
-    //     return bookOptional.orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
-    // }
-
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     @Override
-    public Book createBook(Book book) {
-        return bookRepository.save(book);
+    public List<Book> searchBooks(String keyword) {
+        //            return bookRepository.search(keyword);
+        return bookRepository.findAll();
     }
 
-    // @Override
-    // public Book updateBook(Long id, Book bookD) {
-    //     Book book = getBookById(id);
-    //     book.setTitle(book.getTitle());
-    //     book.setAuthor(book.getAuthor());
-    //     return bookRepository.save(book);
-    // }
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Override
+    public Book findBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Book not found with ID %d", id)));
+    }
 
-    // @Override
-    // public void deleteBookById(Long id) {
-    //     Book book = getBookById(id);
-    //     bookRepository.delete(book);
-    // }
+    @Override
+    public void createBook(Book book) {
+        bookRepository.save(book);
+    }
+
+//    @Override
+//    public void updateBook(Book book) {
+//        bookRepository.save(book);
+//    }
+
+//    @Override
+//    public void deleteBook(Long id) {
+//        final Book book = bookRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException(String.format("Book not found with ID %d", id)));
+//
+//        bookRepository.deleteById(book.getId());
+//    }
+
 }
